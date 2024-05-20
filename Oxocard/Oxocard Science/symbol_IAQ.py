b9ce5db36f83618654d61cc07d491d9edbfb13a7
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-05-20 13:41:27 +0200

    Create symbol_IAQ.py

---
+++ Oxocard/Oxocard Science/symbol_IAQ.py
@@ -0,0 +1,8 @@
+#Symbol IAQ
+def SymbolIAQ():
+    drawLine(-50, -30, 25, -30) 
+    drawArc(30, -60, 30, rad(90), rad(-240))
+    drawLine(-50, 30, 75, 30)
+    drawArc(75, 60, 30, rad(270), rad(260))
+    drawLine(-10, 0, 80, 0)
+    drawLine(-55, 0, -30, 0)
