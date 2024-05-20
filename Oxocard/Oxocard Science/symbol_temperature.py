9760623b9ba81e88fdb721751d12196e4ecce0b2
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-05-20 14:02:08 +0200

    Create symbol_temperature.py

---
+++ Oxocard/Oxocard Science/symbol_temperature.py
@@ -0,0 +1,10 @@
+#Symbol Temperature
+def SymbolTemperature():
+    drawArc(0, 80, 30, rad(225), rad(-270))
+    drawArc(0, -80, 20, rad(180), rad(180))
+    drawLine(-20, -80, -20, 58)
+    drawLine(20, -80, 20, 58)
+    drawLine(30, -70, 50, -70)
+    drawLine(30, -30, 50, -30)
+    drawLine(30, 10, 50, 10)
+    drawLine(30, 50, 50, 50)
