6893303d2f070ed005c1e23e0e7513b6073cf8da
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-05-20 14:18:09 +0200

    Create symbol_ethanol.py

---
+++ Oxocard/Oxocard Science/symbol_ethanol.py
@@ -0,0 +1,13 @@
+#Symbol Ethanol
+def SymbolEthanol():
+    drawCircle(100, 0, 30)
+    drawCircle(-30, -60, 15)
+    drawCircle(30, -60, 15)
+    drawCircle(-30, 60, 15)
+    drawCircle(30, 60, 15)
+    drawCircle(-90, 0, 15)
+    drawCircle(100, -60, 15)
+    drawLine(-30, 50, -30, -50)
+    drawLine(30, 50, 30, -50)
+    drawLine(100, -50, 100, -10)
+    drawLine(-80, 0, 100, 0)
