e3a88cfd64d500415ea107c60a7fe00e535d3071
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-05-20 15:10:09 +0200

    Create symbol_humidity.py

---
+++ Oxocard/Oxocard Science/symbol_humidity.py
@@ -0,0 +1,9 @@
+#Symbol Humidity
+def SymbolHumidity():
+    drawArc(0, 80, 30, rad(225), rad(-270))
+    drawArc(0, -80, 20, rad(180), rad(180))
+    drawLine(-20, -80, -20, 58)
+    drawLine(20, -80, 20, 58)
+    drawArc(60, 30, 26, rad(210), rad(-235))
+    drawLine(38, 17, 60, -30)
+    drawLine(82, 17, 60, -30)
