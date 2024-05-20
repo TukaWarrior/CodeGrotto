436efaa2ede40b1dfe3d2fea6d541520fd58d0b8
Luca Dario Bützberger <luca.buetzberger@gmail.com>
2024-05-20 13:41:59 +0200

    Create symbol_CO2.py

---
+++ Oxocard/Oxocard Science/symbol_CO2.py
@@ -0,0 +1,8 @@
+#Symbol CO2
+def SymbolCO2():
+    drawArc(-60, 30, 50, PI/2, PI/1)
+    drawArc(60, 30, 50, PI/-2, PI/1)
+    drawArc(15, -30, 45, PI/-1.35, PI/1)
+    drawArc(-34, -40, 30, PI/1.4, PI/1)
+    drawArc(-28, 70, 35, PI/1.1, PI/-1.25)
+    drawArc(30, 70, 28, PI/1.15, PI/-1.25)
